330a38d5afd1132ff038141929d889ec22be182c
Vaikus Nikita <vaikusssttark@gmail.com>
2019-06-29 18:35:55 +0300

    Решены новые задачи со Stepic

---
+++ stepik_courses/src/LogTest.java
@@ -0,0 +1,29 @@
+
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.XMLFormatter;
+
+//Реализация объявления логгеров и установления им ограничений. Вывод в консоль в XML-формате.
+
+public class LogTest {
+    public static void main(String[] args) {
+    }
+
+    private static void configureLogging() {
+        Logger loggerA = Logger.getLogger("org.stepic.java.logging.ClassA" );
+        loggerA.setLevel(Level.ALL);
+
+        Logger loggerB = Logger.getLogger("org.stepic.java.logging.ClassB");
+        loggerB.setLevel(Level.WARNING);
+
+        Logger loggerJava = Logger.getLogger("org.stepic.java");
+        loggerJava.setUseParentHandlers(false);
+
+        ConsoleHandler consoleHandler = new ConsoleHandler();
+        consoleHandler.setLevel(Level.ALL);
+        consoleHandler.setFormatter(new XMLFormatter());
+
+        loggerJava.addHandler(consoleHandler);
+    }
+}
